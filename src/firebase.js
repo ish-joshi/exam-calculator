764022a5e240803c754b8d6b0e9ff033970cbd4a
Ishan <ishan.joshi@monash.edu>
2019-05-26 12:48:13 +1000

    add: firebase to project

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+//
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyALKs-szuP968ZynSAsjW8dqAPeXqQndTA",
+    authDomain: "examtime-f4947.firebaseapp.com",
+    databaseURL: "https://examtime-f4947.firebaseio.com",
+    projectId: "examtime-f4947",
+    storageBucket: "examtime-f4947.appspot.com",
+    messagingSenderId: "816336797768",
+    appId: "1:816336797768:web:cbab999ebbd14881"
+  };
+
+  firebase.initializeApp(firebaseConfig)
+
+  export default firebase;
+
+
+//
