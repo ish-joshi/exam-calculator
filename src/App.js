94ea26fd8857bccdc8684399f83f0e549570ea9b
Ishan <ishan.joshi@monash.edu>
2019-05-26 12:43:36 +1000

    add: heading page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography'
+import { Grid } from '@material-ui/core';
+
 
 function App() {
   return (
-    <div>
 
-    </div>
+    <Grid container spacing={16} style={{paddingTop: "5%"}}>
+
+      <Grid item xs={false} sm={1} md={2} />
+      <Grid item xs={12} sm={10} md={8}>
+
+      <Typography align="center" variant="h2" component="h2" gutterBottom>
+        Exam Calculator
+      </Typography>
+
+      </Grid>
+
+    </Grid>
+
+    
+
+
+    
   );
 }
 
